ef0cbbf8a5e4082af388d16dc8130a775c8d3b15
liuhui <liuhuiiris@126.com>
2017-09-22 12:37:21 +0800

    bug fix

--- public/app/features/cmdb/serviceCustomCtrl.ts
+++ public/app/features/cmdb/serviceCustomCtrl.ts
@@ -1,8 +1,5 @@
-///<reference path="../../headers/common.d.ts" />
-
-import angular from 'angular';
 import _ from 'lodash';
-import coreModule from '../../core/core_module';
+import coreModule from 'app/core/core_module';
 
 export class ServiceCustomCtrl {
   softwareList: Array<any>;
