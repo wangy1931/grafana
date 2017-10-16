cf69be74596e1146ffedcea58dc5d8966eb7296d
wxy1995 <wxy1995@users.noreply.github.com>
2017-10-16 00:01:26 -0500

    anomaly metrics ui bug (#145)
    * anomaly metrics, 0-prediction.min displays incorrectly
    * elk dashboard报错后页面挂掉

--- public/app/features/anomaly/anomalyMetric.js
+++ public/app/features/anomaly/anomalyMetric.js
@@ -210,7 +210,7 @@ define([
 
           panel.seriesOverrides[0].alias = alias;
           panel.seriesOverrides[1].alias = metric + ".prediction.min{host=" + hostname + "}";
-          panel.seriesOverrides[1].fill  = 0;
+          panel.seriesOverrides[1].fill  = 5;
           panel.seriesOverrides[1].linewidth  = 0;
           panel.seriesOverrides[2].alias = metric + ".prediction.max{host=" + hostname + "}";
           panel.seriesOverrides[2].fillBelowTo = metric + ".prediction.min{host=" + hostname + "}";
