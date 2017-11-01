55de31645a609358ff4af5ef814575610ebe8424
liuhui <liuhuiiris@126.com>
2017-11-01 20:06:29 +0800

    feature: edit kpi

---
+++ public/app/features/cmdb/metricKpiCtrl.ts
@@ -0,0 +1,10 @@
+///<reference path="../../headers/common.d.ts" />
+
+import angular from 'angular';
+import _ from 'lodash';
+import $ from 'jquery';
+import coreModule from 'app/core/core_module';
+
+export class MetricKpiCtrl {}
+
+coreModule.controller('MetricKpiCtrl', MetricKpiCtrl);
