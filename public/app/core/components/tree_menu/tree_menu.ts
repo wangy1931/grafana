6d30b4ec1cfe501b5e80baa41a5a6b2cde032648
liuhui <liuhuiiris@126.com>
2017-11-06 09:49:06 +0800

    tamplate >> templateURL

--- public/app/core/components/tree_menu/tree_menu.ts
+++ public/app/core/components/tree_menu/tree_menu.ts
@@ -4,56 +4,6 @@ import _ from 'lodash';
 import $ from 'jquery';
 import coreModule from 'app/core/core_module';
 
-var template = `<div ng-init="ctrl.init()" class="tree-menu" ng-class="{true: 'open', false: 'close'}[ctrl.isOpen]">
-  <div class="open-tree">
-    <button ng-click="ctrl.showTree()" class="btn btn-primary btn-isopen">
-      <i class="fa" ng-class="{true: 'fa-chevron-right', false: 'fa-chevron-left'}[ctrl.isOpen]"></i>
-    </button>
-  </div>
-  <div class="tree-container" ng-show="ctrl.isOpen && !ctrl.isLoding">
-    <p class="header">关联指标
-      <i class="fa fa-plus-square-o btn-add-metric" ng-click="ctrl.showNewAssociationManual()"></i>
-      <button class="btn btn-outline-primary pull-right" ng-click="ctrl.clearSelected()">清除选中</button>
-    </p>
-    <div class="tree-content" id="tree-content" ng-show="ctrl.isAssociation">
-      <ul class="tree_ul accordion" id="accordion2">
-        <li class="tree_li" ng-repeat="(service, metrics) in ctrl.correlationMetrics">
-          <a ng-click="ctrl.toggleAccordion($event)" class="tree_node accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#{{_.snakeCase(service)}}">
-            <i class="fa fa-plus-square-o"></i>
-            <p>{{service + ' (' + _.keys(metrics).length + ')'}}</p>
-          </a>
-          <ul id="{{_.snakeCase(service)}}" class="tree_ul accordion-body collapse accordion">
-            <li class="tree_li" ng-repeat="(metric, hosts) in metrics">
-              <a ng-click="ctrl.toggleAccordion($event)" class="tree_node accordion-toggle" data-toggle="collapse" data-parent="#{{_.snakeCase(service)}}" href="#{{_.snakeCase(service) + $index}}">
-                <i class="fa fa-plus-square-o"></i>
-                <p bs-tooltip="'{{_.getMetricName(metric)}}'" data-placement="top" data-container="body">{{_.getMetricName(metric)}}</p>
-              </a>
-              <ul id="{{_.snakeCase(service) + $index}}" class="tree_ul accordion-body collapse">
-                <li class="tree_li" ng-repeat="host in hosts.hosts">
-                  <div class="tree_leaf" ng-click="ctrl.addQuery($event, _.getMetricName(metric), host)">
-                    <input type="checkbox" name="host"
-                      ng-checked="ctrl.checkSource(metric, host)"
-                      ng-disabled="ctrl.checkSource(metric, host)"/>
-                    <p>{{host}}</p>
-                    <span ng-if="!ctrl.checkSource(metric, host)"><i class="fa fa-thumbs-o-up add-rca"></i><i class="fa fa-thumbs-o-down"></i></span>
-                  </div>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-    <div class="tree-content no-data" ng-show="!ctrl.isAssociation">
-      <p>没有关联结果？调整下相似度试试吧。</p>
-      <p>还是没有？请点击"刷新结果"按钮</p>
-      <button class="btn btn-outline-primary" ng-click="ctrl.init()">刷新结果</button>
-    </div>
-    <div class="slider-container"><span>调整相似度</span><div class="sliders" slider></div></div>
-  </div>
-  <cw-loading class="tree-container" ng-if="ctrl.isLoding" show="ctrl.isLoding" duration=6000></cw-loading>
-</div>`;
-
 export class TreeMenuCtrl {
   isAssociation: boolean;
   isOpen: boolean;
@@ -261,7 +211,7 @@ export function treeMenu() {
     controller: TreeMenuCtrl,
     bindToController: true,
     controllerAs: 'ctrl',
-    template: template,
+    templateUrl: 'public/app/core/components/tree_menu/tree_menu.html',
     link: (scope, elem, attrs, ctrl) => {
       ctrl.panel = scope.$parent.panel;
     }
