f94206e6bcb78cbe1acb30981673c02754e837c6
liuhui <liuhuiiris@126.com>
2017-11-20 12:22:51 +0800

    custom report

--- public/app/features/report/reportCtrl.ts
+++ public/app/features/report/reportCtrl.ts
@@ -3,20 +3,29 @@
 import config from 'app/core/config';
 import coreModule from 'app/core/core_module';
 import _ from 'lodash';
+import 'ng-quill';
 
 export class ReportCtrl {
   reports: Array<any>;
   reportTemplate: any;
+  customReport: any;
+  toolbarOptions: any;
 
   /** @ngInject */
   constructor (private $scope, private backendSrv, private contextSrv) {}
 
+  /**
+   * get report list
+   */
   init() {
     this.backendSrv.get('/api/static/template/' + this.contextSrv.user.orgId).then((result) => {
       this.reports = result.reports;
     });
   }
 
+  /**
+   * eidt report template
+   */
   initTemplate() {
     this.reportTemplate = {
       isOpen: false,
@@ -59,10 +68,34 @@ export class ReportCtrl {
     console.log(this.reportTemplate);
   }
 
-
-  initEdit() {}
-
   switch() {}
+
+  /**
+   * edit custom report
+   */
+
+  initEdit() {
+    this.toolbarOptions = [
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+      [{ 'direction': 'rtl' }],                         // text direction
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'align': [] }],
+      ['image'],
+      ['clean']                                         // remove formatting button
+    ];
+  }
+
+  editorCreated() {}
+  saveCustomReport() {
+    console.log(this.customReport);
+  }
+
 }
 
 coreModule.controller('ReportCtrl', ReportCtrl);
