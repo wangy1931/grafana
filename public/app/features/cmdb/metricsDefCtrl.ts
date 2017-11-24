aa40cf80872d13e8aedba78a5ee8e7ebe53163ba
liuhui <liuhuiiris@126.com>
2017-11-24 17:53:10 +0800

    指标概览接口调整

--- public/app/features/cmdb/metricsDefCtrl.ts
+++ public/app/features/cmdb/metricsDefCtrl.ts
@@ -12,50 +12,54 @@ export class MetricsDefCtrl {
   typeList: Array<any>;
   query: any;
   params: any;
+  suggestMetrics: any;
 
   /** @ngInject */
-  constructor(private $scope, private backendSrv, private contextSrv) {
+  constructor(private $scope, private metricSrv, private contextSrv) {
     this.params = {
       size: SIZE,
     };
     this.clearQuery();
-    this.typeList = [{
-      type: '系统',
-      subType: [
-        'CPU',
-        'IO',
-        'JVM',
-        '内存',
-        '存储',
-        '网络',
-        '运行状态'
-      ]
-    },{
-      type: '服务',
-      subType: []
-    }];
-    this.getService();
+    this.getSubtype();
   }
 
-  getService() {
-    this.backendSrv.alertD({
-      url: '/cmdb/service'
-    }).then((res) => {
-      _.each(res.data, (service) => {
-        this.typeList[1].subType.push(service.name);
+
+  getSubtype() {
+    this.metricSrv.getSubtype().then((res) => {
+      this.typeList = res.data;
+      _.each(this.typeList, (item) => {
+        if (item.sysId) {
+          item.name = '系统';
+        } else {
+          item.name = '服务';
+        };
+        item.software = _.uniqBy(item.software, 'name');
       });
     });
   }
 
   getMetricsList(page, query?) {
+    this.params.name && delete this.params.name;
+    this.params.type && delete this.params.type;
+    this.params.subType && delete this.params.subType;
+    delete this.params.kpi;
+    delete this.params.custom;
     if (query) {
       page = 1;
       if (query.metric) {
         this.params.name = query.metric;
-      } else if (query.type) {
-        this.params.type = query.type.type;
-        if (query.subType) {
-          this.params.subtype = query.subtype;
+      } else {
+        if (query.type) {
+          this.params.type = query.type.name;
+          if (query.subType) {
+            this.params.subtype = query.subType.name;
+          }
+        }
+        if (_.isNumber(query.isKpi)) {
+          this.params.kpi = query.isKpi;
+        }
+        if (_.isBoolean(query.isCustom)) {
+          this.params.custom = query.isCustom;
         }
       }
     } else if (page < 1) {
@@ -64,7 +68,7 @@ export class MetricsDefCtrl {
       return;
     }
     this.params.page = page;
-    this.backendSrv.getMetricInfo(this.params).then((res) => {
+    this.metricSrv.getMetricInfo(this.params).then((res) => {
       if (this.params.name) {
         if (_.isNull(res.data)) {
           this.metricList = [];
@@ -86,11 +90,10 @@ export class MetricsDefCtrl {
       this.metricCur.id = -1;
       return;
     }
-    this.backendSrv.getMetricInfo({id: metric.id}).then((res) => {
-      this.metricCur = res.data;
-      this.metricCur.disabled = true;
-      this.metricCur.subTypes = _.find(this.typeList, {type: this.metricCur.type}).subType;
-    });
+    this.metricCur = _.cloneDeep(metric);
+    this.metricCur.disabled = true;
+    this.metricCur.subTypes = _.find(this.typeList, { name: this.metricCur.type }).software;
+    this.metricCur.subType = _.find(this.metricCur.subTypes, { name: metric.subType });
   }
 
   edit() {
@@ -98,14 +101,19 @@ export class MetricsDefCtrl {
     this.metricEdit = _.cloneDeep(this.metricCur);
   }
 
-  update() {
+  update(metric) {
     this.metricCur.disabled = true;
+    delete this.metricCur.subTypes;
+    var tmp = this.metricCur.subType;
+    this.metricCur.subType = this.metricCur.subType.name;
     var params = {
       id: this.metricCur.id,
       userId: this.contextSrv.user.id
     };
-    this.backendSrv.updateMetricInfo(params, this.metricCur).then((res) => {
+    this.metricSrv.updateMetricInfo(params, this.metricCur).then((res) => {
       this.$scope.appEvent('alert-success', ['保存成功']);
+      this.metricCur.subType = tmp;
+      metric = _.cloneDeep(this.metricCur);
     }, () => {
       this.cancel();
       this.$scope.appEvent('alert-danger', ['保存失败']);
@@ -123,11 +131,21 @@ export class MetricsDefCtrl {
       type: null,
       subType: ''
     };
-    this.params.name && delete this.params.name;
-    this.params.type && delete this.params.type;
-    this.params.subType && delete this.params.subType;
     this.params.page = 1;
     this.getMetricsList(this.params.page);
+  }
+
+  getSuggest(query) {
+    this.metricSrv.getSuggest(query).then((res) => {
+      this.suggestMetrics = res.data;
+      _.each(this.suggestMetrics, (suggest, index) => {
+        this.suggestMetrics[index] = _.getMetricName(suggest);
+      });
+    });
+  }
+
+  changeType() {
+    this.query.subType = this.query.type.software[0];
   }
 }
 
