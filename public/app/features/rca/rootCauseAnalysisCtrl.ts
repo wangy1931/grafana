6c3d32bbc4ff337cf9da329238ce369f18bc1779
wxy1995 <wxy1995@users.noreply.github.com>
2017-11-30 14:32:41 +0800

    总览 UI REDESIGN & RCA LOG (#169)
    * elk table & rca add log

--- public/app/features/rca/rootCauseAnalysisCtrl.ts
+++ public/app/features/rca/rootCauseAnalysisCtrl.ts
@@ -14,7 +14,10 @@ export class RootCauseAnalysisCtrl {
   traceList: Array<string> = [];
 
   /** @ngInject */
-  constructor(private backendSrv, private $location, private $scope, private $rootScope) {
+  constructor(
+    private backendSrv, private popoverSrv,
+    private $location, private $scope, private $rootScope, private $timeout
+  ) {
     this.toolkit = window.jsPlumbToolkit.newInstance();
     this.renderer = this.renderFactory();
 
@@ -74,9 +77,6 @@ export class RootCauseAnalysisCtrl {
         canvasElement = mainElement.querySelector(`.jtk-canvas`),
         miniviewElement = mainElement.querySelector(".miniview");
 
-    // reset canvas height
-    $(".jtk-canvas").css({ "height": window.innerHeight - 52 });
-
     return this.toolkit.render({
       container: canvasElement,
       view: {
@@ -90,21 +90,10 @@ export class RootCauseAnalysisCtrl {
         },
         nodes: {
           "default": {
+            template: "tmplNode",
             events: {
-              tap: (params) => {
-                this.resetGraph();
-                this.renderer.selectAllEdges({
-                  element: params.el
-                }).addClass('unselected');
-                $('.jtk-node').not(params.el).addClass('unselected');
-
-                // search
-                var searchParams = _.extend({}, this.$location.search(), {
-                  metric: params.el.getAttribute("data-jtk-node-id")
-                });
-                this.$location.search(searchParams);
-              },
-              click: this.nodeClickHandler.bind(this)
+              tap: this.nodeTapHandler.bind(this),
+              click: this.nodeClickHandler.bind(this),
             }
           }
         }
@@ -186,17 +175,57 @@ export class RootCauseAnalysisCtrl {
     });
   }
 
-  showGuideResult(e, params) {
-    var selectors = $(`[data-jtk-node-id="${params.metric}"]`)
-    if (selectors.length) {
-      this.resetGraph();
-      this.renderer.selectAllEdges({
-        element: selectors[0]
-      }).addClass('unselected');
-      $('.jtk-node').not(selectors[0]).addClass('unselected');
+  nodeTapHandler(params) {
+    this.resetGraph();
+    this.renderer.selectAllEdges({
+      element: params.el
+    }).addClass('unselected');
+    $('.jtk-node').not(params.el).addClass('unselected');
 
-      selectors[0].click();
+    // search
+    var searchParams = _.extend({}, this.$location.search(), {
+      metric: params.el.getAttribute("data-jtk-node-id")
+    });
+    this.$location.search(searchParams);
+
+    // show node details
+    this.$scope.detail = {
+      name: params.node.data.name,
+      type: params.node.data.type,
+      description: []
+    };
+    if (params.node.data.desc) {
+      try {
+        this.$scope.detail.description = JSON.parse(params.node.data.desc);
+      } catch (e) {
+        this.$scope.detail.description = [
+          { '描述': params.node.data.desc }
+        ];
+      }
+    } else {
+      this.$scope.detail.description = [
+        { '描述': params.node.data.desc }
+      ];
     }
+    this.$scope.$digest();
+  }
+
+  showGuideResult(e, params) {
+    this.$timeout(() => {
+      var selectors = $(`[data-jtk-node-id="${params.metric}"]`);
+      var node = this.toolkit.getNode(params.metric);
+
+      if (selectors.length && node) {
+        this.nodeTapHandler({
+          el: selectors[0],
+          node: node
+        });
+        selectors[0].click();
+      }
+    }, 100);
+  }
+
+  showNodeDetail(node) {
   }
 };
 
