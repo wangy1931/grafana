ea7b83e0423e66be680314829d22dfb8c36641ec
liuhui <liuhuiiris@126.com>
2017-12-05 17:21:38 +0800

    指标名称提示

--- public/app/features/logs/logParseEditCtrl.ts
+++ public/app/features/logs/logParseEditCtrl.ts
@@ -58,6 +58,8 @@ export class LogParseEditCtrl {
     }
     if (_.isEqual(logType, '其他')) {
       return;
+    } else {
+      this.custom.logType = '';
     }
     var params = {
       logServiceName: logServiceName
@@ -184,6 +186,7 @@ export class LogParseEditCtrl {
       } else {
         newScope.isNew = false;
         newScope.pattern = _.cloneDeep(pattern);
+        newScope.pattern.fields = [];
         newScope.oldPattern = pattern;
         newScope.rule = this.rule;
       }
