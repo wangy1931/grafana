10eadf428a03a1c829dfa951e5ebd1cb9cdf74fe
travelling-wxy <travel2wxy@163.com>
2018-01-22 14:24:12 +0800

    fix confilice

--- public/app/core/components/navbar/navbar.ts
+++ public/app/core/components/navbar/navbar.ts
@@ -3,16 +3,21 @@
 import config from 'app/core/config';
 import _ from 'lodash';
 import $ from 'jquery';
+import moment from 'moment';
 import coreModule from '../../core_module';
 
 export class NavbarCtrl {
   showGuideNav: boolean = false;
   showNavbarPageBtn: boolean = false;
+  deadline: Number;
+  priceUrl: string;
 
   /** @ngInject */
   constructor(private $scope, private $rootScope, private $location, private contextSrv) {
     !!~['/rca', '/association', '/logs', '/topn'].indexOf(this.$location.path()) && (this.showGuideNav = true);
     this.showNavbarPageBtn = (this.$location.path() === '/');
+    this.deadline = moment(contextSrv.user.deadline).diff(moment(), 'days');
+    this.priceUrl = '//cloudwiz.cn/product_price.html';
   }
 
   showGuide() {
